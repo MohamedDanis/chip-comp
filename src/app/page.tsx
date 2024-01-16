7d607afa90311c888c66f436731b17736b534858
Mohamed Danis <62685219+MohamedDanis@users.noreply.github.com>
2024-01-16 15:55:47 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,131 @@
-import Image from 'next/image'
+"use client"
+import Image from "next/image";
+import React, { useState, useRef, useEffect, KeyboardEvent } from "react";
+
+interface Option {
+  id: number;
+  name: string;
+  profile:string;
+}
+import img from '../../public/imgs/avatar1.jpg'
 
 export default function Home() {
+  const [selected, setSelected] = useState<Option[]>([]);
+  const [showOptions, setShowOptions] = useState(false);
+  const [options, setOptions] = useState<Option[]>([
+    { id: 1, name: "John",profile:'/imgs/avatar1.jpg' },
+    { id: 2, name: "dani",profile:'/imgs/avatar2.jpg' },
+    { id: 3, name: "shahi",profile:'/imgs/avatar3.jpg' },
+    { id: 4, name: "abhi",profile:'/imgs/avatar4.jpg' },
+    { id: 5, name: "amar",profile:'/imgs/avatar1.jpg' },
+    { id: 6, name: "binu",profile:'/imgs/avatar2.jpg' },
+  ]);
+  const [search, setSearch] = useState("");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [deleteIndex, setDeleteIndex] = useState(-1);
+
+  const handleSelect = (option: Option) => {
+    const newSelected = [...selected];
+    if (newSelected.find((o) => o.id === option.id)) {
+      newSelected.splice(
+        newSelected.findIndex((o) => o.id === option.id),
+        1
+      );
+    } else {
+      newSelected.push(option);
+    }
+    setSelected(newSelected);
+    if (inputRef.current) {
+      inputRef.current.focus();
+      setSearch("");
+    }
+  };
+
+  const handleDeselect = (option: Option) => {
+    const newSelected = [...selected];
+    newSelected.splice(
+      newSelected.findIndex((o) => o.id === option.id),
+      1
+    );
+    setSelected(newSelected);
+  };
+
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Backspace" && search === "") {
+      setDeleteIndex(selected.length - 1);
+    }
+  };
+
+  const handleDelete = (option: Option) => {
+    const newSelected = [...selected];
+    newSelected.splice(
+      newSelected.findIndex((o) => o.id === option.id),
+      1
+    );
+    setSelected(newSelected);
+  };
+
+  useEffect(() => {
+    if (deleteIndex >= 0) {
+      const option = selected[deleteIndex];
+      handleDelete(option);
+      setDeleteIndex(-1);
+    }
+  }, [deleteIndex]);
+
+  const filteredOptions = options.filter((o) => {
+    return (
+      !selected.find((s) => s.id === o.id) &&
+      o.name.toLowerCase().includes(search.toLowerCase())
+    );
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="w-full h-screen bg-slate-100 flex flex-col items-center gap-y-12 justify-center">
+      <div className="w-full flex flex-col items-center justify-center max-w-[400px] mx-auto relative">
+        <div className="flex flex-wrap p-2 bg-white gap-y-3 w-full  border-b-2 border-blue-800" onKeyDown={handleKeyDown} >
+          {selected.map((option, index) => (
+            <div
+              key={option.id}
+              className={`${index === deleteIndex ? "bg-gray-200" : ""} mr-2 px-2 py-1 bg-blue-800 rounded-full flex items-center`}
+            >
+              {option.name}
+              <span
+                className="ml-2 cursor-pointer"
+                onClick={() => handleDeselect(option)}
+              >
+                x
+              </span>
+            </div>
+          ))}
+          <input
+            className="outline-none flex-1 text-black w-[60px] bg-none "
+            value={search}
+            ref={inputRef}
+            onClick={() => setShowOptions(true)}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search"
+          />
         </div>
+        {showOptions && (
+          <div className="w-full mt-3">
+            <select className="border p-2 text-black w-full rounded-md outline-none" multiple>
+              {filteredOptions.map((option) => (
+                <option
+                  className="p-2 hover:bg-gray-100 flex gap-2"
+                  key={option.id}
+                  onClick={() => handleSelect(option)}
+                  selected={!!selected.find((o) => o.id === option.id)}
+                >
+                  <div className="bg-[url('../../public/imgs/avatar1.jpg')] rounded-full w-14 h-14"></div>
+                  {option.name}
+                </option>
+              ))}
+            </select>
+          </div>
+        )}
+       
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
